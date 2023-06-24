5cff6b9150dadb88511d1085aeb818ccbaca8306
AnushkaGautam02 <124994205+AnushkaGautam02@users.noreply.github.com>
2023-06-24 19:40:11 +0530

    Add files via upload

---
+++ database/data.js
@@ -0,0 +1,56 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+    {
+        id:1,
+        question: "What is the name of one of the Neo-Aramaic languages spoken by the Jewish population from Northwestern Iraq?",
+        options: [
+            "Hulaul&aacute;",
+            "Lishan Didan",
+            "Lishana Deni",
+            "Chaldean Neo-Aramaic"
+        ]
+    },
+    {
+        id:2,
+        question: "What was the first interactive movie video game?",
+        options: [
+            "Cube Quest",
+            "M.A.C.H. 3",
+            "Astron Belt",
+            "Dragon&#039;s Lair"
+        ]
+    },
+    {
+        id:3,
+        question: "In Need For Speed: Most Wanted (2005), how many people are there to defeat on the blacklist?",
+        options: [
+            "10",
+            "20",
+            "5",
+            "15"
+        ]
+    },
+    {
+        id:4,
+        question: "What is the 4th boss in the 1997 video game &quot;Crash Bandicoot 2: Cortex Strikes Back&quot;?",
+        options: [
+            "Dr. Neo Cortex",
+            "Tiny Tiger",
+            "Komodo Brothers",
+            "Dr. N. Gin"
+        ]
+    },
+    {
+        id:5,
+        question: "In The Simpsons, which war did Seymour Skinner serve in the USA Army as a Green Beret?",
+        options: [
+            "Vietnam War",
+            "World War 2",
+            "Cold War",
+            "World War 1"
+        ]
+    }
+]
+
+
+export const answers = [1,2,3,3,0];
